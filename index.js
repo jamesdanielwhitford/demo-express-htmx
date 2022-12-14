ffbc8dfa86b8d4afd114c24d2ac70ee1f13f7015
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-12-14 11:26:54 +0200

    css

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.set('view engine', 'pug');
 // application routes
 app.use("/api", bookRoutes.routes);
