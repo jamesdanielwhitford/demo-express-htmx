ef142a3ec4e41d671017959eb857a7e3f5db244c
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-12-14 09:24:20 +0200

    Add css

--- test.js
+++
@@ -1,6 +0,0 @@
-// if env variable x == True make 
-if (process.env.x == 'pizza') {
-    document.querySelector('h1').innerHTML = 'Pizza Reccomendations';
-  } else {
-    document.querySelector('h1').innerHTML = 'Book Reccomendations';
-  }
